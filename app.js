ddbb2ac5473134397f3f021e9af87b03a0fc771a
Manoj Kshetrapal <kshetrapalmanoj@gmail.com>
2021-04-20 12:29:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,4 +30,4 @@ app.get('/',(req,res)=>{
     res.send('Welcome')
 })
 
-app.listen(3000,()=>console.log("Server is running at 3000"))
+app.listen(process.env.PORT || 3000,()=>console.log("Server is running at 3000"))
