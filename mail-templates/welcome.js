e69ca07a886fe61cdc48fb7388f3bb03d7db5288
Yash Mutha <muthayashm@gmail.com>
2021-05-08 18:11:51 +0530

    Added api for reset link mail & reset password, used nodemailer for welcome mail & reset-link mail

---
+++ mail-templates/welcome.js
@@ -0,0 +1,154 @@
+var template = (name) => `
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title>Reset Password Email Template</title>
+    <meta name="description" content="Reset Password Email Template." />
+    <style type="text/css">
+      a:hover {
+        text-decoration: underline !important;
+      }
+    </style>
+  </head>
+
+  <body
+    marginheight="0"
+    topmargin="0"
+    marginwidth="0"
+    style="margin: 0px; background-color: #f2f3f8"
+    leftmargin="0"
+  >
+    <!--100% body table-->
+    <table
+      cellspacing="0"
+      border="0"
+      cellpadding="0"
+      width="100%"
+      bgcolor="#f2f3f8"
+      style="
+        @import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700);
+        font-family: 'Open Sans', sans-serif;
+      "
+    >
+      <tr>
+        <td>
+          <table
+            style="background-color: #f2f3f8; max-width: 670px; margin: 0 auto"
+            width="100%"
+            border="0"
+            align="center"
+            cellpadding="0"
+            cellspacing="0"
+          >
+            <tr>
+              <td style="height: 80px">&nbsp;</td>
+            </tr>
+            <tr>
+              <td style="text-align: center">
+                <img
+                  width="50%"
+                  src="https://i.imgur.com/J8MGSEb.png"
+                  title="logo"
+                  alt="logo"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td style="height: 20px">&nbsp;</td>
+            </tr>
+            <tr>
+              <td>
+                <table
+                  width="95%"
+                  border="0"
+                  align="center"
+                  cellpadding="0"
+                  cellspacing="0"
+                  style="
+                    max-width: 670px;
+                    background-color: #f2f3f8;
+                    border-radius: 15px;
+                    text-align: center;
+                    -webkit-box-shadow: 0 6px 18px 0 rgba(0, 0, 0, 0.06);
+                    -moz-box-shadow: 0 6px 18px 0 rgba(0, 0, 0, 0.06);
+                    box-shadow: 0 6px 18px 0 rgba(0, 0, 0, 0.06);"
+                >
+                  <tr>
+                    <td style="height: 40px">&nbsp;</td>
+                  </tr>
+                  <tr>
+                    <td style="padding: 0 35px">
+                      <h1
+                        style="
+                          color: #67adf5;
+                          font-weight: bold;
+                          margin: 0;
+                          font-size: 32px;
+                          font-family: 'Rubik', sans-serif;
+                        "
+                      >
+                        Kudos ! You are now a part of World's Biggest Developer
+                        Directory
+                      </h1>
+                      <span
+                        style="
+                          display: inline-block;
+                          vertical-align: middle;
+                          margin: 29px 0 26px;
+                          border-bottom: 1px solid #cecece;
+                          width: 250px;
+                        "
+                      ></span>
+                      <p
+                        style="
+                          color: #455056;
+                          font-size: 15px;
+                          line-height: 24px;
+                          margin: 0;
+                        "
+                      >
+                        A warm welcome and lots of good wishes on becoming part
+                        of our growing community. Congratulations and on behalf
+                        of all the members. We are all happy and excited about
+                        your suggestions and contribution to our community.
+                      </p>
+
+                      <a
+                        href="http://localhost:3000/browse"
+                        style="
+                          background: #20e277;
+                          text-decoration: none !important;
+                          font-weight: 500;
+                          margin-top: 35px;
+                          color: #fff;
+                          font-size: 14px;
+                          padding: 10px 24px;
+                          display: inline-block;
+                          border-radius: 50px;
+                        "
+                      >
+                        Browse <strong>Community</strong> now !</a
+                      >
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="height: 40px">&nbsp;</td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+
+            <tr>
+              <td style="height: 20px">&nbsp;</td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+    <!--/100% body table-->
+  </body>
+</html>
+`
+
+module.exports = template
