808c8d185a3adc616231f6c59f36b8e14f1ed9e5
Yash Mutha <muthayashm@gmail.com>
2021-05-08 18:01:02 +0530

    Added api for reset link mail & reset password, used nodemailer for welcome mail & reset-link mail

--- model/Developer.js
+++ model/Developer.js
@@ -1,21 +1,31 @@
 const mongoose = require('mongoose');
 const developerScheme = new mongoose.Schema({
-    full_name:{
-        type:String,
-        required:true,
+    full_name: {
+        type: String,
+        required: true,
     },
-    email:{
-        type:String,
-        required:true,
+    email: {
+        type: String,
+        required: true,
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    group:{
-        type:String,
-        required:true
+    group: {
+        type: String,
+        required: true
+    },
+    reset: {
+        resetToken: {
+            type: String,
+            required: false
+        },
+        tokenExpiry: {
+            type: Date,
+            required: false
+        }
     }
 });
 
-module.exports  = mongoose.model('developers',developerScheme);
+module.exports = mongoose.model('developers', developerScheme);
